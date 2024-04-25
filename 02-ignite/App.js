8255603deb8ede390bc0d94300a8f1d7bb13aa2a
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-04-25 19:31:18 +0530

    ignite

---
+++ 02-ignite/App.js
@@ -0,0 +1,31 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello world from react"
+// );
+
+/* <div id="parent">
+  <div id="child">
+    <h1>this is react code</h1>
+    <h2>from namaste react</h2>
+  </div>
+</div>; */
+
+//createElement creates object
+
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "im learning react"),
+    React.createElement("h2", {}, "from namaste react"),
+  ])
+);
+
+console.log(parent); // object
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent); //render takes heading (object) and convert into tag
