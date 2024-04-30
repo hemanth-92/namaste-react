92d7ca870570be1fb8a52d2785498de4f0068514
hemanth-92 <131354764+hemanth-92@users.noreply.github.com>
2024-04-30 19:16:13 +0530

    finally done

--- 03-foundation/App.js
+++ 03-foundation/App.js
@@ -1,24 +1,40 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 //jsx --- html or xml like syntax
-//jsx is converted before reaching js engine - parcel(manager) -babel(package)
+//jsx is converted before reaching js engine - parcel(manager) -babel(package) js complier
 //jsx => babel converts React.createElement => ReactcreateElement Object => HTMLElement(render)
 
 //recat functional components
 //js function with jsx is called funtional components
 
+const elem = <span>jsx</span>;
+
 const Title = () => {
-  <h1>Hello world</h1>;
+  return (
+    <h2>
+      {elem}
+      Hello world
+    </h2>
+  );
 };
 
 // component composition => component inside in a component
+// code is readable bcz of jsx
+
+const name = "nix vs aux";
 
 const HeadingComponent = () => {
-  <div>
-    <Title />
-    <h1>hello how r u </h1>;
-  </div>;
+  return (
+    <div>
+      <Title />
+      <Title></Title>
+      {Title()}
+      <h2>{name}</h2>
+      <h2>{449 / 47}</h2>
+      <h3>hello how r u </h3>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
